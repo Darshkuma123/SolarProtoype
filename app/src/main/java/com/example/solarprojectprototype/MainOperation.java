ba528bcc6c8936023bc4540483a79b06834ad7cb
Darshkuma123 <drashanyad19@gmail.com>
2023-01-22 00:01:28 +0530

    filtered

--- app/src/main/java/com/example/solarprojectprototype/MainOperation.java
+++ app/src/main/java/com/example/solarprojectprototype/MainOperation.java
@@ -8,12 +8,17 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainOperation extends AppCompatActivity {
-
+    Button StartConversion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_logic);
+        StartConversion=findViewById(R.id.StartConversion);
+        StartConversion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-
+            }
+        });
     }
 }
